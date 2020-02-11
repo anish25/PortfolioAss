5c82a8b873034a51f9152caba58f3f04866e2e99
Anish Gandhi <anishgandhi25@gmail.com>
2020-02-11 14:26:51 -0500

    Second commit after setting up routes and nav bar

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET About page. */
+router.get('/about', (req, res, next) => {
+  res.render('about', { title: 'About Page' });
+});
+
+/* GET projects page. */
+router.get('/projects', (req, res, next) => {
+  res.render('projects', { title: 'Projects Page' });
+});
+
+/* GET Contact page. */
+router.get('/contact', (req, res, next) => {
+  res.render('contact', { title: 'Contact Us Page' });
+});
+
 module.exports = router;
